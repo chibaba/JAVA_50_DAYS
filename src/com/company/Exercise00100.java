a1961294af8297ad9c5b2aa2df75c490a3c0250d
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-04-30 21:07:18 +0100

    linkedHashedset

---
+++ src/com/company/Exercise00100.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Exercise00100 {
+    public static void main (String[] args) {
+        Set set1 = new LinkedHashSet();
+        set1.add(35);
+        set1.add(19);
+        set1.add(20);
+        set1.add(29);
+        set1.add(2);
+        System.out.println(set1);
+
+    }
+}
