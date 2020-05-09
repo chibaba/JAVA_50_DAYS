675196c6a4049baf0c8601ca3b1635bc620e8d07
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-05-09 16:52:49 +0100

    using input to analyze data

---
+++ src/com/company/AnalyzeInput.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.Console;
+import java.util.*;
+
+public class AnalyzeInput {
+    public static void main(String[] args) {
+        ArrayList <String> text = new ArrayList<String>();
+        Console cons;
+        String line = "";
+        while (!line.equals("*")
+        && (cons = System.console()) != null
+        && (line = cons.readLine()) !=null) {
+            List<String> lineList = new ArrayList<String>(Arrays.asLIst(line.split("")));
+            text.addAll(lineList);
+        }
+        System.out.println("You typed: "+ text);
+        System.out.println("Word Count: "+ text.size());
+        Set <String> textSet = new HashSet<String>();
+        textSet.addAll(text);
+        Iterator iterator = textSet.iterator();
+        while (iterator.hasNext()) {
+            //point to next element
+            String s    = (String) iterator.next();
+            //get the amount of time this word shows up in the text
+            int freq = Collections.frequency(text, s);
+            //print out the result
+            System.out.println(s + " appears " + freq + " times");
+        }
+    }
+}
