cb43b0a0e8b1d90c8b2d5d7e0dafcc0f2e35e772
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-05-13 17:58:21 +0100

    nullpointer exception

---
+++ src/com/workinprogress/Example03.java
@@ -0,0 +1,21 @@
+package com.workinprogress;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class Example03 {
+    public static void main (String[] args) {
+        // declare a list that will contain all of the files
+        // inside of the readme.txt file
+        List<String> lines = Collections.emptyList();
+        //provoke an exception
+        //lines = Files.readAllLines(Paths.get("readme.txt"));
+        //you will never see this print
+        Iterator<String> iterator = lines.iterator();
+        while(iterator.hasNext())
+            System.out.println(iterator.next());
+    }
+}
---
+++ src/com/workinprogress/Example04.java
@@ -0,0 +1,14 @@
+package com.workinprogress;
+
+public class Example04 {
+    public static void main (String[] args) {
+        String  vehicleType = null;
+        String  vehicle = "car";
+
+        if (vehicle.equals(vehicleType)) {
+            System.out.println("its a car");
+        } else {
+            System.out.println("its not a car");
+        }
+    }
+}
