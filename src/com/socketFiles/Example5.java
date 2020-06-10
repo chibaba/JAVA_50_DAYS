7482791a13a053ad6dbb39b318322e3b28c2aab1
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-10 18:21:31 +0100

    working with sub directories

---
+++ src/com/socketFiles/Example5.java
@@ -0,0 +1,18 @@
+package com.socketFiles;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Example5 {
+    public static void main(String[] args) throws IOException {
+        String pathString = System.getProperty("user.home");
+        List<Path> subDirectories = Files.walk(Paths.get(pathString), 1)
+                .filter(Files::isDirectory)
+                .collect(Collectors.toList());
+        for ()
+    }
+}
