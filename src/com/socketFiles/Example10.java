d08b6e47c360d83a29d532678703b4fdd33c97df
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-13 10:17:43 +0100

    reading a property file

--- src/com/socketFiles/Example10.java
+++ src/com/socketFiles/Example10.java
@@ -9,6 +9,18 @@ public class Example10 {
         Properties properties = new Properties();
         properties.load(fileStream);
         System.out.println("name: "+ properties.getProperty("name"));
-        System.out.println("family name: " + properties.getProperty("familyName"))
+        System.out.println("family name: " + properties.getProperty("familyName"));
+        System.out.println("nick: "+ properties.getProperty("username"));
+        System.out.println("age: "+ properties.getProperty("age"));
+        System.out.println("background color: "+properties.getProperty("bgColor"));
+    }
+
+    public static void main(String[] args) throws IOException {
+        String pathString = System.getProperty("user.home") + "/javaTemp/user.properties";
+        FileInputStream fileStream = null;
+        try {
+            fileStream = new FileInputStream(pathString);
+            PrintOutProperties(fileStream);
+        } catch
     }
 }
