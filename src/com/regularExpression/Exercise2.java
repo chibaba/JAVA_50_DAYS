858dc456d693a38e945fece67f5b915c432a9253
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-17 22:43:34 +0100

    extrext part component and print to the terminal

--- src/com/regularExpression/Exercise2.java
+++ src/com/regularExpression/Exercise2.java
@@ -6,15 +6,18 @@ import java.util.regex.Pattern;
 public class Exercise2 {
     public static void main(String[] args) {
         String url = "https://www.packtpub.com/application-development/mastering-java-9";
-        String regex = "(https[s]?) (?:://) ([w]{0,3}\\.?[a-zA-Z]+\\.[a-zA-Z]{2,3})";
+        String regex = "(https[s]?) (?:://) ([w]{0,3}\\.?[a-zA-Z]+\\.[a-zA-Z]{2,3}) (?:[/]) (.*)";
+        System.out.println(regex);
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(url);
         boolean foundMatches = matcher.find();
         if (foundMatches) {
             String protocol = matcher.group(1);
             String domain = matcher.group(2);
+            String path = matcher.group(3);
             System.out.println("Protocol: " + protocol);
             System.out.println("domain: " + domain);
+            System.out.println("Path: " + path);
         }
     }
 }
