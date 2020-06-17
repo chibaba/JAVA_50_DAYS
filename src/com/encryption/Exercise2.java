845d66f2d9e6dab44d3ef401a54278c906a4a0fc
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-17 04:56:57 +0100

    encryption

--- src/com/encryption/Exercise2.java
+++ src/com/encryption/Exercise2.java
@@ -1,8 +1,10 @@
 package com.encryption;
 
-import javax.crypto.Cipher;
+import javax.crypto.*;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 
 public class Exercise2 {
     public static void main(String[] args) {
@@ -12,7 +14,24 @@ public class Exercise2 {
             KeyPair KeyPair = KeyPairGenerator.getInstance(algorithm).generateKeyPair();
             String transformation = algorithm + "/ECB/PKCS1Padding";
             Cipher cipher = Cipher.getInstance(transformation);
-            cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
+            cipher.init(Cipher.ENCRYPT_MODE, KeyPair.getPublic());
+            byte[] cipherText = new byte[cipher.getOutputSize(plainText.getBytes().length)];
+            int encryptedLength = cipher.update(plainText.getBytes(), 0, plainText.getBytes().length,
+                    cipherText);
+            cipher.doFinal(cipherText, encryptedLength);
+            System.out.println(new String (cipherText));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (ShortBufferException e) {
+            e.printStackTrace();
         }
     }
 }
