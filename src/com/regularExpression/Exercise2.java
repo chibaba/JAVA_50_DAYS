0b30edf6a2cc840d7b7dfbd691ca74f475c3d589
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-17 22:01:12 +0100

    extracting domain using patterns

---
+++ src/com/regularExpression/Exercise2.java
@@ -0,0 +1,8 @@
+package com.regularExpression;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        String url = "https://www.packtpub.com/application-development/mastering-java-9";
+        String regex = "(https[s]?)";
+    }
+}
