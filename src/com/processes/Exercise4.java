23776851f9c7a88a12b1821962089a1800946418
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-18 22:24:40 +0100

    launching  PROCESS

--- src/com/processes/Exercise4.java
+++ src/com/processes/Exercise4.java
@@ -1,11 +1,20 @@
 package com.processes;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 public class Exercise4 {
     public static void main(String[] args) {
         ProcessBuilder processBuilder = new ProcessBuilder("firefox");
         Process process = null;
         try {
-            process
+            process = processBuilder.start();
+        } catch (IOException ioe) {
+            System.out.println("WARNING: interruption happened with exec");
+
+        }
+        try {
+            process.waitFor(10, TimeUnit.SECONDS)
         }
     }
 }
