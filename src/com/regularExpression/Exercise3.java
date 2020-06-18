ec05698c2cafc50cc5dd4502cf684c76bddc23f8
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-18 20:34:22 +0100

    wip

--- src/com/regularExpression/Exercise3.java
+++ src/com/regularExpression/Exercise3.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Exercise3 {
     public static void main(String[] args) {
@@ -11,6 +13,8 @@ public class Exercise3 {
         try  {
             String packtDump = new String(Files.readAllBytes(Paths.get(filePath)));
             String regex = "(?:<a href=\") ([^\"] +)(?:\"{1})";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(packtDump);
         } catch (IOException e) {
             e.printStackTrace();
         }
