90534bd06c7e131dbc02ba380be4f3dd9efe8519
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-24 18:15:09 +0100

    immutable class

--- src/com/Lambda/Exercise2.java
+++ src/com/Lambda/Exercise2.java
@@ -1,20 +1,38 @@
 package com.Lambda;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise2 {
     public static void main(String[] args ) {
 
     }
 
-    public static final class ShoppingCart{
+    public static final class ShoppingCart {
 
-    }
-    private static final class ShoppingItem {
-        private final String name;
-        private final int price;
 
-        public ShoppingItem(String name, int price) {
-            this.name  = name;
-            this.price = price;
+        private static final class ShoppingCart {
+            public final List<ShoppingItem> myShoppingList;
+
+            public ShoppingCart(List<ShoppingItem> list) {
+                myShoppingList = collections.unmodifiableList(list)
+            }
+
+            public ShoppingCart addItem(ShoppingItem item) {
+                List<ShoppingItem> newList = new ArrayList<>(myShoppingList);
+                newList.add(item);
+                return new ShoppingCart(newList)
+            }
+        }
+
+        private static final class ShoppingItem {
+            private final String name;
+            private final int price;
+
+            public ShoppingItem(String name, int price) {
+                this.name = name;
+                this.price = price;
+            }
         }
     }
 }
