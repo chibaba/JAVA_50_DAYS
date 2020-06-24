2d954e47ec685a36f4deba69620b6741c24fcdf7
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-24 17:35:02 +0100

    created lambda file

---
+++ src/com/Lambda/Exercise2.java
@@ -0,0 +1,20 @@
+package com.Lambda;
+
+public class Exercise2 {
+    public static void main(String[] args ) {
+
+    }
+
+    public static final class ShoppingCart{
+
+    }
+    private static final class ShoppingItem {
+        private final String name;
+        private final int price;
+
+        public ShoppingItem(String name, int price) {
+            this.name  = name;
+            this.price = price;
+        }
+    }
+}
