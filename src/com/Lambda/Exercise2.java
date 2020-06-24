cefa534e8d951efc7367d0114809ed058638a807
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-24 21:27:49 +0100

    immutable collection

--- src/com/Lambda/Exercise2.java
+++ src/com/Lambda/Exercise2.java
@@ -1,14 +1,21 @@
 package com.Lambda;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+//Add a new behavior to your ShoppingCart:
+//
+//        Create a removeItem(ShoppingItem) function.
+//        Create a function that takes multiple ShoppingItem as arguments, either as a list or as variable arguments.
+//        Modify your ShoppingCart to take multiple items of each ShoppingItem, for example, four chairs and one table. Additionally, modify the addItem(ShoppingItem) and removeItem(ShoppingItem) functions.
+//        Note: If you get stuck, remember that you can find full solution files at the end of the course.
+
+import com.Lambda.Exercise2.ShoppingCart.ShoppingItem;
+
+import java.util.*;
 
 public class Exercise2 {
-    public static void main(String[] args ) {
+    public static void main(String[] args) {
         ShoppingCart myFirstCart = new ShoppingCart(new ArrayList<>());
         ShoppingCart mySecondCart = myFirstCart.addItem(new ShoppingItem("laptop", 234));
-        ShoppingCart myThirdCart = mySecondCart.addItem(new ShoppingItem( "break", 600));
+        ShoppingCart myThirdCart = mySecondCart.addItem(new ShoppingItem("break", 600));
 
         System.out.println("First" + myFirstCart);
         System.out.println("Second" + mySecondCart);
@@ -19,17 +26,37 @@ public class Exercise2 {
 
     public static final class ShoppingCart {
 
-            public final List<ShoppingItem> myShoppingList;
+        public final List<ShoppingItem> myShoppingList;
 
-            public ShoppingCart(List<ShoppingItem> list) {
-                myShoppingList = Collections.unmodifiableList(list);
-            }
+        public ShoppingCart(List<ShoppingItem> list) {
+            myShoppingList = Collections.unmodifiableList(list);
+        }
 
-            public ShoppingCart addItem(ShoppingItem item) {
-                List<ShoppingItem> newList = new ArrayList<>(myShoppingList);
-                newList.add(item);
-                return new ShoppingCart(newList);
+        public ShoppingCart removeItem(ShoppingItem item) {
+            Map<String, Integer> newList = new HashMap<>(myShoppingList);
+
+            int value = 0;
+            if (newList.containsKey(item.name)) {
+                value = newList.get(item.name);
             }
+            if (value > 0) {
+                newList.put(item.name, --value);
+            }
+            return new ShoppingCart(newList);
+        }
+
+        public ShoppingCart addItem(ShoppingItem item) {
+            Map<String, Integer> newList = new HashMap<>(myShoppingList);
+            ShoppingCart newCart = null;
+            for (ShoppingItem item : items) {
+                newCart = addItem(item);
+            }
+            return newCart;
+
+
+    }
+
+
 
         @Override
         public String toString() {
