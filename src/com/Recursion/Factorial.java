4b7d157903a2e2c4993dda6bc03c279972530bbc
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-26 11:28:54 +0100

    working with big integer

--- src/com/Recursion/Factorial.java
+++ src/com/Recursion/Factorial.java
@@ -4,12 +4,16 @@ import java.math.BigInteger;
 
 import static java.math.BigDecimal.ONE;
 
-public class FactorialCalculator {
+public class Factorial {
     public static int factorial(int number) {
         if( number <= 1) {
             return BigInteger.ONE.intValue();
         } else {
             return number * factorial(number - 1);
         }
+    }
+    public static BigInteger factorial(BigInteger number) {
+
+
     }
 }
---
+++ src/com/Recursion/FactorialTest.java
@@ -0,0 +1,30 @@
+package com.Recursion;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.math.BigInteger;
+
+class FactorialTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+    @Test
+    void factorialTest() {
+        int answer = Factorial.factorial(5);
+        assertEquals(120,answer)
+    }
+
+    private void assertEquals(int i, int answer) {
+    }
+    @Test
+    void FactorialWithBigInteger() {
+        BigInteger = o
+    }
+}
