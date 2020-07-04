980156284a437f962de891ebb16c886d0a5f32d5
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-05 00:08:06 +0100

    using the strem apis too

--- src/com/streams/ShoppingCart.java
+++ src/com/streams/ShoppingCart.java
@@ -6,6 +6,15 @@ import java.util.List;
 
 public class ShoppingCart {
     public static void main(String[] args) {
+        ShoppingCart fruitCart = new ShoppingCart(List.of(
+                new ShoppingArticle("Orange", 1.5),
+                new ShoppingArticle("Apple", 1.7),
+                new ShoppingArticle("Banana", 2.2)
+        ));
+        ShoppingCart vegetableCart = new ShoppingCart(List.of(
+                new ShoppingArticle("cucumber", 0.8),
+                new ShoppingArticle()
+        ))
 
     }
 
