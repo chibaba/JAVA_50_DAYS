ebe73b59293a0b72803ab5ebc974b62cd8f78d62
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-05 17:51:49 +0100

    doing specific dearch

--- src/com/streams/SpecificSearch.java
+++ src/com/streams/SpecificSearch.java
@@ -1,4 +1,24 @@
 package com.streams;
+//Build a function that will find the cheapest fruit from a list of ShoppingArticles.
+//        Build a function that will find the most expensive vegetable from a list of ShoppingArticles.
+//        Build a function that will gather all fruits in a separate list.
+//        Build a function that will find the five least expensive articles in the database.
+//        Build a function that will find the five most expensive articles in the database.
+
 
 public class SpecificSearch {
+    final String name;
+    final String  Category;
+    final double price;
+    final String unit;
+    public static void main(String[] args) {
+
+    }
+    private SpecificSearch(String name, String Category, double price, String unit) {
+        this.name = name;
+        this.Category = Category;
+        this.price = price;
+        this.unit = unit;
+
+    }
 }
