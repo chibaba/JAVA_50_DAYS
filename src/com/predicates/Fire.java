a7a1c4893a8a4e8cb73219e8635e9184735f9ae5
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-06 22:57:29 +0100

    fire and movt

---
+++ src/com/predicates/Fire.java
@@ -0,0 +1,21 @@
+package com.predicates;
+
+public class Fire implements Sensor {
+    private int batteryHealth;
+    private boolean triggered;
+    private int temperature;
+
+    @Override
+    public int batteryHealth() {
+        return batteryHealth;
+    }
+    @Override
+       public void batteryHealth(int health) {
+
+    }
+    @Override
+    public void triggered(boolean state)
+}
+public int temperature() {
+    return temperature;
+}
