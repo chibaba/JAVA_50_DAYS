6aaada92b74a67b6bc2a89a1d060c2cb1719f0d8
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-07 19:36:34 +0100

    decoding files to single words

--- src/com/ReactiveProgramming/Processor.java
+++ src/com/ReactiveProgramming/Processor.java
@@ -1,7 +1,22 @@
 package com.ReactiveProgramming;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Processor {
     public static void main(String[] args) {
 
+    }
+    private static String[] getStrings() {
+      String filePath = "res/numbers.txt";
+      try (Stream<String> words =
+              Files.lines(Paths.get(filePath))) {
+
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
     }
 }
