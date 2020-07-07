6b396efb3913f505cb765871d31f576794ba4fe8
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-07 18:22:00 +0100

    call back executed by publisher when req is issued by publishers

--- src/com/ReactiveProgramming/LipsumSubscriber.java
+++ src/com/ReactiveProgramming/LipsumSubscriber.java
@@ -6,10 +6,13 @@ public class LipsumSubscriber implements Flow.Subscriber<String> {
     private  Flow.Subscription subscription;
     @Override
     public void onSubscribe(Flow.Subscription subscription) {
+        this.subscription = subscription;
+        this.subscription.request(1);
 
     }
     @Override
     public void onNext (String item) {
+        System.out.println(item);
 
     }
     @Override
