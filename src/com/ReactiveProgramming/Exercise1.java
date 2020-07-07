159bd88e7d963a5b8a2fe755cd51e1c838777f1d
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-07 18:04:58 +0100

    submission publisher

--- src/com/ReactiveProgramming/Exercise1.java
+++ src/com/ReactiveProgramming/Exercise1.java
@@ -1,4 +1,9 @@
 package com.ReactiveProgramming;
 
+import java.util.concurrent.SubmissionPublisher;
+
 public class Exercise1 {
+    public static void main(String[]  args) {
+        SubmissionPublisher<String> publisher = new SubmissionPublisher<>();
+    }
 }
