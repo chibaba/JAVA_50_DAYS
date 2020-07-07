9db0bbb7b17168386602c20aa4cfe3e736454805
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-07 23:48:01 +0100

    let the supplier cal the getString

--- src/com/ReactiveProgramming/Processor.java
+++ src/com/ReactiveProgramming/Processor.java
@@ -5,18 +5,57 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.concurrent.Flow;
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class Processor {
     public static void main(String[] args) {
         Supplier<String> supplier = new Supplier <String>() {
+            int index;
             @Override
             public String get() {
                 String[] data = getStrings();
-                return null;
+                if (index < data.length -1) {
+
+                    return data[index++];
+                } else {
+                    return  null;
+                }
             }
         };
+        NumberPublisher publisher = new NumberPublisher(supplier);
+        publisher.subscribe(new Subscriber<String>() {
+            Flow.Subscription subscription;
+            @Override
+            public void onSubscribe(Flow.Subscription subscription) {
+                this.subscription = subscription;
+                subscription.request(1);
+
+            }
+
+            @Override
+            public void onNext(String item) {
+                System.out.println("onNext: " + item);
+                subscription.request(1);
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("onError: " +
+                        throwable.getMessage());
+
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete()");
+
+            }
+        });
 
     }
     private static String[] getStrings() {
