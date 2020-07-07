b138500588005202d0bc33f1b264524de3b0a7a8
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-07 19:10:11 +0100

    we want publisher to shut down as well as timer

--- src/com/ReactiveProgramming/NumberPublisher.java
+++ src/com/ReactiveProgramming/NumberPublisher.java
@@ -1,6 +1,19 @@
 package com.ReactiveProgramming;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.SubmissionPublisher;
 
 public class NumberPublisher extends SubmissionPublisher <String> {
+    final Timer timer = new Timer();
+    final TimerTask timerTask = new TimerTask() {
+        @Override
+        public void run() {
+
+        }
+    };
+    public NumberPublisher() {
+
+    }
+
 }
